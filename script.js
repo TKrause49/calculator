75ab924e50e64629728a39da5c290979fdc6bd09
Trey Krause <tjkrause49@gmail.com>
2023-11-06 19:56:28 -0500

    Create functions for basic math operations
    Added functions to add, subtract, multiply, and divide two numbers.
    
    Added operator function that determines which function to call.

---
+++ script.js
@@ -0,0 +1,28 @@
+let firstNumber = 6;
+let operator = '*';
+let secondNumber = 2;
+
+function add (first, second) {
+    return first + second;
+}
+
+function subtract (first, second) {
+    return first - second;
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+    return first / second;
+}
+
+function operate (first, op, second) {
+    if (op === '+') {return add(first, second)};
+    if (op === '-') {return subtract(first, second)};
+    if (op === '*') {return multiply(first, second)};
+    if (op === '/') {return divide(first, second)};
+}
+
+console.log(operate(firstNumber, operator, secondNumber));
